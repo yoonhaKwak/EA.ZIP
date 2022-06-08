5f696e2fc0c4eb8e1f60a7bc756c8b79ebf3d331
hi <schaser0326@daum.net>
2022-06-08 16:51:27 +0900

    yyc test EA_ZIP

---
+++ src/main/java/com/example/ea_zip1/EaZip1Application.java
@@ -0,0 +1,13 @@
+package com.example.ea_zip1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaZip1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaZip1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ea_zip1/EaZip1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ea_zip1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaZip1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
