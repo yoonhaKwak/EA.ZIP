e606321b058fc59e08ee71ed6e56c77b848fc2f5
yoonhaKwak <106727633+yoonhaKwak@users.noreply.github.com>
2022-07-06 15:22:41 +0900

    kyh

---
+++ EAZIP/src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import axios from "axios";
+import TestApi from './TestApi';
+
+const App = () =>  {
+
+  return (
+        <TestApi/>
+  );
+}
+
+export default App;
