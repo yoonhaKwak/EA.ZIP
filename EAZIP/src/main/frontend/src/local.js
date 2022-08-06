dcfb7cbbbcb94353b9da8be6f79bc7084ce7e5ad
doj427 <101680314+doj427@users.noreply.github.com>
2022-08-06 15:53:06 +0900

    khj_0806

---
+++ EAZIP/src/main/frontend/src/local.js
@@ -0,0 +1,47 @@
+import React, {useEffect} from "react";
+import axios from "axios";
+import {useState} from "react";
+
+function Filter(){
+    const [addr1, setAddr1] = useState("논현로");
+
+    const [TimeSectionMin, setTimeSectionMin] = useState(500000);
+    const [mip, setMip] = useState(1000);
+    const [mam, setMam] = useState(0);
+    const [mim, setMim] = useState(0);
+
+    const [data, setData] = useState('안됨');
+
+    const Back = async () => {
+        axios(
+            {
+                url: '/local/apilist',
+                method: 'post',
+                data: {
+                    "addr1" : addr1,
+                    'TimeSectionMin' : 5000,
+                    "TimeSectionMax" : 2000,
+                    "WalkTimeMin" : 3000,
+                    "WalkTimeMax" : 2000,
+                    "TransferMin" : 5 ,
+                    "TransferMax" : 3,
+                },
+                baseURL :'http://localhost:8080'
+            }
+        ).then(response => setData(JSON.stringify(response.data)))
+        {
+            console.log("WOW 드디어 8080 됐다~")
+        };
+    }
+
+    return(
+        <div>
+            <button onClick={Back}>데이터 보내기</button>
+            <div>
+                <p>테스트</p>
+                {data}
+            </div>
+        </div>
+    )
+}
+export default Filter;
